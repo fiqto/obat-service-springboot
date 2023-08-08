ef3c0e66a70cd93e67d720ca4e2ad106f53f7fca
Fiqri Baihaqi Hermawanto <58502780+fiqto@users.noreply.github.com>
2023-08-09 02:03:44 +0700

    Update ObatServiceApplication.java

--- src/main/java/com/obat/obatservice/ObatServiceApplication.java
+++ src/main/java/com/obat/obatservice/ObatServiceApplication.java
@@ -2,8 +2,14 @@ package com.obat.obatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@ComponentScan
 public class ObatServiceApplication {
 
 	public static void main(String[] args) {
